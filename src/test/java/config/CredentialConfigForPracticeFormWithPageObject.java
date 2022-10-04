ed8d83db35afe10d5ccdf431015e37c0154b5a7b
Mariya Bykova <bykova.m@gmail.com>
2022-10-04 19:07:59 +0700

    Initial commit

---
+++ src/test/java/config/CredentialConfigForPracticeFormWithPageObject.java
@@ -0,0 +1,9 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:config/credentialForPracticeFormWithPageObject.properties")
+public interface CredentialConfigForPracticeFormWithPageObject extends Config {
+    String login();
+    String password();
+}
